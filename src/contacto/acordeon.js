c2138680464878b8dc3a43fe2906fceb3bc07ea8
ponchisao326_ <ponchisao326@hotmail.com>
2025-05-22 20:17:58 +0200

    contacto y footer modificado

---
+++ src/contacto/acordeon.js
@@ -0,0 +1,7 @@
+// FAQ acordeón simple
+document.querySelectorAll('.faq-question').forEach(q => {
+    q.addEventListener('click', function() {
+        const item = this.parentElement;
+        item.classList.toggle('active');
+    });
+});
